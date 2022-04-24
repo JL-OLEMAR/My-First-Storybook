e20fe161c24af9858f338be6d18f6985707226d1
JL-OLEMAR <joseluis19963@gmail.com>
2022-04-24 02:29:26 -0500

    fix: github actions configured

--- dist/stories/Page.stories.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-declare const _default: ComponentMeta<import("react").VFC<{}>>;
-export default _default;
-export declare const LoggedOut: ComponentStory<import("react").VFC<{}>>;
-export declare const LoggedIn: ComponentStory<import("react").VFC<{}>>;
